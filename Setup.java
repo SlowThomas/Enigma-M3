8782f00b9d889478a75fe374c0a46a982c6cbaa3
SlowThomas <94425945+SlowThomas@users.noreply.github.com>
2023-06-12 14:32:04 -0400

    Update setup

--- Setup.java
+++ Setup.java
@@ -1,5 +1,5 @@
 public class Setup{
-    public String plugboard = "AC BF HG JS";
-    public String rotor_set = "I III IV UKW-B";
-    public String rotor_pos = "N J R";
+    public String plugboard = "AC BF HG JS VL XQ IO PZ MN UK RE";
+    public String rotor_set = "I III II UKW-B";
+    public String rotor_pos = "E X T";
 }
